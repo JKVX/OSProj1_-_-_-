d4da302a3f816a2e9a40f1b20996ddf571eed892
JKVX <357800013@qq.com>
2015-12-10 09:14:59 +0800

    Update thread.h

--- thread.h
+++ thread.h
@@ -92,7 +92,11 @@ struct thread
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
-
+    
+    /*add by jinxin for alarm-clock*/
+    int64_t ticks_blocked;
+    /*add e*/
+    
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
@@ -115,6 +119,10 @@ void thread_print_stats (void);
 
 typedef void thread_func (void *aux);
 tid_t thread_create (const char *name, int priority, thread_func *, void *);
+
+/*add by jinxin for alarm-clock*/
+void blocked_thread_check(struct thread *t,void *aux UNUSED);
+/*add e*/
 
 void thread_block (void);
 void thread_unblock (struct thread *);
