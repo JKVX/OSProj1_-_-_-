1fdfcde7b0bf3baa7d98744960ada0ce33309da1
JKVX <357800013@qq.com>
2015-12-10 09:24:20 +0800

    Update timer.c

--- timer.c
+++ timer.c
@@ -84,17 +84,24 @@ timer_elapsed (int64_t then)
   return timer_ticks () - then;
 }
 
+/*modify by jinxin for alarm-clock*/
 /* Sleeps for approximately TICKS timer ticks.  Interrupts must
-   be turned on. */
+   be turned on. */ 
 void
 timer_sleep (int64_t ticks) 
 {
-  int64_t start = timer_ticks ();
-
-  ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+  if(ticks <=0)
+{
+   return;
 }
+  ASSERT (intr_get_level () == INTR_ON);
+  enum intr_level old_level = intr_disable();
+  struct thread *current_thread = thread_current();
+  current_thread->ticks_blocked=ticks;
+  thread_block ();
+  intr_set_level (old_level);
+}
+/*modify e*/
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
    turned on. */
@@ -171,6 +178,11 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
+  
+  /*modify by jinxin for alarm-clock*/
+  thread_foreach (blocked_thread_check,NULL);
+  /*modify e*/
+  
   thread_tick ();
 }
 
